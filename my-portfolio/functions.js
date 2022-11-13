fd38eccb958bc53d5c1474e7acd236a222c6aef7
Freddy Tomada <freddytomada7@gmail.com>
2022-11-13 12:50:30 -0400

    add proyect-portfolio

---
+++ my-portfolio/functions.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    let firstDiv = document.getElementById("firstDiv")
+    let imgFront = document.getElementById("imgFront")
+
+    firstDiv.classList.add("divMove")
+    imgFront.classList.add("divScale")
+}
